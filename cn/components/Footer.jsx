c0884164f6d6842b2c1040d4817890106544298c
James Gu <james_gu14@hotmail.com>
2016-08-14 22:53:22 +0800

    Footer, Nav, Top bar for home page

---
+++ cn/components/Footer.jsx
@@ -0,0 +1,113 @@
+import React, { Component } from 'react'
+import { Link, IndexLink } from 'react-router'
+import NavLink from './NavLink.jsx'
+
+import TopBar from './TopBar.jsx'
+import NavBar from './NavBar.jsx' 
+import Home from '../pages/Home.jsx'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="trd-footer-top">
+              <div className="col-md-3 col-sm-6 col-xs-12 trd-footer-widget trd-about-widget">
+                <div className="trd-logo-wrapper">
+                  <img src="/content/images/logo-footer.png" alt="Trade" />
+                </div>
+
+                <p>澳洲飛揚投資有限公司（For Young Investments  Pty  Ltd， ACN 609 081 932 )是位於澳大利亞南澳州首府阿德萊德（Adelaide）的著名公司。</p>
+              </div>
+
+              <div className="col-md-3 col-sm-6 col-xs-12 trd-footer-widget trd-useful-link-widget">
+                <h3 className="trd-footer-widget-title">常用鏈接</h3>
+
+                <div className="trd-footer-widget-content">
+                  <ul>
+                    <li><NavLink to="/cn">首頁</NavLink></li>
+                    <li><NavLink to="/cn/about">關於我們</NavLink></li>
+                    <li><NavLink to="/cn/products">產品</NavLink></li>
+                    <li><NavLink to="/cn/payment">在線支付</NavLink></li>
+                    <li><NavLink to="/cn/tracking">包裹追蹤</NavLink></li>
+                    <li><NavLink to="/cn/contact">聯繫我們</NavLink></li>
+                  </ul>
+                </div>
+              </div>
+
+              <div className="col-md-3 col-sm-6 col-xs-12 trd-footer-widget trd-recent-post-widget">
+                <h3 className="trd-footer-widget-title">Recent Post</h3>
+
+                <div className="trd-footer-widget-content">
+                  <div className="trd-recent-post-wrapper">
+                    <div className="trd-recent-post-item">
+                      <h3 className="trd-post-title">
+                        <a href="#">Financial express anounced the share list from candites</a>
+                      </h3>
+
+                      <span className="trd-post-date">April 25, 2016</span>
+                    </div>
+
+                    <div className="trd-recent-post-item">
+                      <h3 className="trd-post-title">
+                        <a href="#">Financial express anounced the share list from candites</a>
+                      </h3>
+
+                      <span className="trd-post-date">April 25, 2016</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 col-sm-6 col-xs-12 trd-footer-widget trd-subscribe-widget">
+                <h3 className="trd-footer-widget-title">訂閱我們</h3>
+
+                <div className="trd-footer-widget-content">
+                  <p>訂閱我們的最新消息，以及各種優惠的商業活動 </p>
+
+                  <form action="#">
+                    <input type="email" name="trd-subscription" id="trd-subscription" placeholder="您的郵箱" />
+                    <button type="submit" className="trd-btn">訂閱郵件</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+
+            <div className="trd-footer-bottom">
+              <div className="trd-footer-menu">
+                <ul>
+                  <li>
+                    <IndexLink to="/cn/" activeClassName="active">首頁</IndexLink>
+                  </li>
+                  <li>
+                    <NavLink to="/cn/about">關於我們</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/cn/products">產品</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/cn/payment">在線支付</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/cn/tracking">包裹追蹤</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/cn/contact">聯繫我們</NavLink>
+                  </li>
+                  <li>
+                    <a href="/">English</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="trd-copyright-info">
+                <p>Copyright © 2016 by <a href="#" className="trd-author-link">James Gu</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    )
+  }
+}
