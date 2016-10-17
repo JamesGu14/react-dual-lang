4b7a1101bcf36bc91a875f4696ce2f082bce45cd
James Gu <james_gu14@hotmail.com>
2016-10-17 12:41:37 +0800

    side link

--- cn/components/home/SideLink.jsx
+++ cn/components/home/SideLink.jsx
@@ -5,20 +5,21 @@ export default class SideLink extends Component {
   render() {
     return (
       <div className="sidelink-container">
-        
         <ul>
-          <li className="dropdown">
-            <a href="#" data-toggle="dropdown">First Menu <i className="icon-arrow"></i></a>
-            <ul className="dropdown-menu">
-              <li><a href="#">Home</a></li>
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Services</a></li>
-              <li><a href="#">Contact</a></li>
-              <NavLink to="/cn/contact" className="trd-btn trd-btn-alt">聯繫我們</NavLink>
+          <li className="sidelink-dropdown">
+            <a href="#" className="hide-link" id="hide-sidelink">&nbsp;&nbsp;隐藏</a>
+            <a href="#" data-toggle="sidelink-dropdown">快速联系</a>
+            <ul className="sidelink-dropdown-menu show">
+              <li><a href="#">在线客服</a></li>
+              <li><a href="#">客服电话</a></li>
+              <li><a href="#">官方二维码</a></li>
+              <li><a href="#">活动新闻</a></li>
+              <li><a href="#">商业客户</a></li>
+              <li><a href="#">网点联系</a></li>
+              <li><a href="#" className="sidelink-footer">返回顶部</a></li>
             </ul>
           </li>
         </ul>
-        <script src="/content/js/vendors/sidelink.js"></script>
       </div>
     )
   }
--- content/js/sidelink.js
+++ content/js/sidelink.js
@@ -1,29 +1,34 @@
 // Dropdown Menu
-var dropdown = document.querySelectorAll('.dropdown')
+var dropdown = document.querySelectorAll('.sidelink-dropdown')
 var dropdownArray = Array.prototype.slice.call(dropdown, 0)
 dropdownArray.forEach(function (el) {
-  var button = el.querySelector('a[data-toggle="dropdown"]'),
-    menu = el.querySelector('.dropdown-menu'),
-    arrow = button.querySelector('i.icon-arrow')
+  var button = el.querySelector('a[data-toggle="sidelink-dropdown"]')
+  var menu = el.querySelector('.sidelink-dropdown-menu')
+  var arrow = button.querySelector('i.icon-arrow')
+    
 
   button.onclick = function (event) {
-		if (!menu.hasClass('show')) {
-			menu.classList.add('show')
-			menu.classList.remove('hide')
-			arrow.classList.add('open')
-			arrow.classList.remove('close')
-			event.preventDefault()
-		}
-		else {
-			menu.classList.remove('show')
-			menu.classList.add('hide')
-			arrow.classList.remove('open')
-			arrow.classList.add('close')
-			event.preventDefault()
-		}
-	}
+    if (!menu.hasClass('show')) {
+      menu.classList.add('show')
+      menu.classList.remove('hide')
+      arrow.classList.add('open')
+      arrow.classList.remove('close')
+      event.preventDefault()
+    }
+    else {
+      menu.classList.remove('show')
+      menu.classList.add('hide')
+      arrow.classList.remove('open')
+      arrow.classList.add('close')
+      event.preventDefault()
+    }
+  }
 })
 
 Element.prototype.hasClass = function (className) {
-	return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className)
+  return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className)
 }
+
+$('#hide-sidelink').click(function() {
+  $('.sidelink-container').hide()
+})
